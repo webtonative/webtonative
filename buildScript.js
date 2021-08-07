7fd03d136b0559de5c38cc8d134fc107aac8f190
Himanshu Khantwal <hkhantwal7@gmail.com>
2021-08-07 22:07:14 +0530

    version changed

--- buildScript.js
+++ buildScript.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+fs.copyFileSync("README.md","build/README.md");
 const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 delete packageJson.private;
 fs.writeFileSync("build/package.json",JSON.stringify(packageJson,null,4))
