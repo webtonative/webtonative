82f456f0abd152d099b8ff6f977aa672c5b1dd34
Vikash Sharma <>
2021-11-25 14:54:04 +0530

    Update Barcode scan code

--- src/barcode/barcode-scan.js
+++ src/barcode/barcode-scan.js
@@ -1,5 +1,12 @@
-import { isNativeApp, platform, webToNative, registerCb } from "../utills";
+import {
+  isNativeApp,
+  platform,
+  webToNative,
+  registerCb,
+  webToNativeIos,
+} from "../utills";
 let successCb = null;
+import Format from "./formats";
 /**
  * This function opens native barcode scanner from Android/Ios app.
  * @param {object} options
@@ -10,20 +17,25 @@ let successCb = null;
  * });
  */
 export default (options) => {
-  if (isNativeApp && platform === "ANDROID_APP") {
+  if (["ANDROID_APP", "IOS_APP"].includes(platform)) {
+    const { onBarcodeSearch, format } = options;
+
     registerCb((response) => {
       const { type, value } = response;
       if (type === "BARCODE_SCAN") {
-        successCb(value);
+        onBarcodeSearch && onBarcodeSearch(value);
       }
     });
-    const { onBarcodeSearch, formats = [] } = options;
-    const barcodeOptions = {
-      formats,
-    };
-    isNativeApp && webToNative.startScanner(JSON.stringify(barcodeOptions));
-    if (typeof onBarcodeSearch === "function") {
-      successCb = onBarcodeSearch;
-    }
+    platform === "ANDROID_APP" &&
+      webToNative.startScanner(
+        JSON.stringify({
+          formats: format ? [format] : [],
+        })
+      );
+    platform === "IOS_APP" &&
+      webToNativeIos.postMessage({
+        action: "barcodeScan",
+        barcodeFormat: format ? String(format) : String(Format.ALL_FORMATS),
+      });
   }
 };
