92322d2da38b65617743802472ccf33ca9e73555
Himanshu Khantwal <35111854+hkhantwal7@users.noreply.github.com>
2022-12-15 13:43:30 +0530

    1.0.23

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const production = process.env.NODE_ENV == "production";
 const path = require("path");
-const fs = require('fs');
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const fs = require("fs");
+const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
 const version = packageJson.version;
 module.exports = {
-  mode: production ? "production" : "development",
-  ...(!production && { devtool: "source-map" }),
-  entry: {
-    'webtonative': "./client.js",
-  },
-  output: {
-    path: path.resolve("./dist"),
-    filename: `[name].${version}${production ? ".min" : ""}.js`,
-    environment : {
+	mode: production ? "production" : "development",
+	...(!production && { devtool: "source-map" }),
+	entry: {
+		webtonative: "./client.js",
+	},
+	output: {
+		path: path.resolve("./build"),
+		filename: `[name]${production ? ".min" : ""}.js`,
+		environment: {
 			arrowFunction: false,
 			bigIntLiteral: false,
 			const: false,
@@ -20,20 +20,25 @@ module.exports = {
 			dynamicImport: false,
 			forOf: false,
 			module: false,
-		}
-  },
-  module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: 'babel-loader',
-			options: {
-				presets:[
-          ['@babel/preset-env',{
-            modules:false
-          }]
-        ]
-			}
-		}]
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				options: {
+					presets: [
+						[
+							"@babel/preset-env",
+							{
+								modules: false,
+							},
+						],
+					],
+				},
+			},
+		],
 	},
 };
