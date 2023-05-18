053d434e7b235277ddbe009e2cacb11f900efea4
Venkatesh Mohta <venkatesh_mohta@hotmail.com>
2023-05-18 22:48:07 +0530

    Bottom Navigation Handing Android

--- src/BottomNavigation/index.js
+++ src/BottomNavigation/index.js
@@ -5,6 +5,9 @@ import { platform, webToNative, webToNativeIos } from "../utills";
  */
 export const hide = () => {
 	if (["ANDROID_APP", "IOS_APP"].includes(platform)) {
+		platform === "ANDROID_APP" &&
+        	webToNative.showHideStickyFooter(false);
+
 		platform === "IOS_APP" &&
 			webToNativeIos.postMessage({
 				action: "showHideStickyFooter",
@@ -20,6 +23,9 @@ export const hide = () => {
 export const show = (options = {}) => {
 	const { key } = options;
 	if (["ANDROID_APP", "IOS_APP"].includes(platform)) {
+		platform === "ANDROID_APP" &&
+        	webToNative.showHideStickyFooter(true);
+
 		platform === "IOS_APP" &&
 			webToNativeIos.postMessage({
 				action: "showHideStickyFooter",
