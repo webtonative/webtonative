bf593571107d49d55962f570aa95f0fe90d4e015
Venkatesh Mohta <venkatesh_mohta@hotmail.com>
2025-06-04 17:50:38 +0530

    WIP

--- src/Stripe/index.js
+++ src/Stripe/index.js
@@ -2,7 +2,7 @@ import { platform, webToNative, webToNativeIos, registerCb } from "../utills";
 
 export const makeTapToPay = (options) => {
     if (["ANDROID_APP","IOS_APP"].includes(platform)) {
-        const { callback, apiUrl=null, amount, currency, isSimulated=false, captureMethod="automatic", connectionToken, stripeLocationId } = options;
+        const { callback, apiUrl=null, amount, currency, isSimulated=false, captureMethod="automatic", connectionToken, stripeLocationId, clientSecret=null } = options;
         registerCb((response) => {
             const { type } = response;
             if (type === "makeTapToPayStripePayment") {
@@ -20,6 +20,9 @@ export const makeTapToPay = (options) => {
         }
         if(apiUrl){
             paymentData.apiUrl = apiUrl
+        }
+        if(clientSecret){
+            paymentData.client_secret = clientSecret
         }
         
         platform === "ANDROID_APP" && webToNative.makeTapToPayStripePayment(JSON.stringify(paymentData));
