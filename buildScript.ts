4c138d25d53747248bfd292a488384ef9fd77da0
vijay <vijay.pandey@orufy.com>
2025-08-27 10:53:00 +0530

    add : build changes

--- buildScript.ts
+++ buildScript.ts
@@ -1,11 +1,35 @@
-const fs = require('fs');
+const fs = require("fs");
+import path from "path";
 
 // Copy README.md to build folder
 fs.copyFileSync("README.md", "build/README.md");
 
 // Read package.json, remove private flag, and write to build folder
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
 delete packageJson.private;
+
+const buildPath = path.resolve("build");
+
+const exportsMap = {
+	".": {
+		import: "./build/index.js",
+		types: "./build/index.d.ts",
+	},
+};
+
+const entries = fs.readdirSync(buildPath, { withFileTypes: true });
+
+for (const entry of entries) {
+	if (entry.isDirectory()) {
+		const name = entry.name;
+		exportsMap[`./${name}`] = {
+			import: `./build/${name}/index.js`,
+			types: `./build/${name}/index.d.ts`,
+		};
+	}
+}
+
+packageJson.exports = exportsMap;
 
 // Write the modified package.json to build folder
 fs.writeFileSync("build/package.json", JSON.stringify(packageJson, null, 4));
