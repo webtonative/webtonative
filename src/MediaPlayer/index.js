062adf1e4407444f751bc7017a4ec58f2c64e5ab
vijay <vijay.pandey@orufy.com>
2025-09-19 11:32:12 +0530

    add : cb in media player

--- src/MediaPlayer/index.js
+++ src/MediaPlayer/index.js
@@ -4,16 +4,27 @@ import { platform, registerCb, webToNative, webToNativeIos } from "../utills";
  *
  */
 export const playMedia = (options = {}) => {
-	const { url, imageUrl = "https://images.freeimages.com/images/large-previews/3b2/prague-conference-center-1056491.jpg"  } = options;
+	const {
+		url,
+		imageUrl = "https://images.freeimages.com/images/large-previews/3b2/prague-conference-center-1056491.jpg",
+		callback,
+	} = options;
 	if (["ANDROID_APP", "IOS_APP"].includes(platform)) {
-		
-		platform === "ANDROID_APP" && webToNative.playMedia(JSON.stringify({url,imageUrl}));
-
-		platform === "IOS_APP" && webToNativeIos.postMessage({
-			"action": "playMedia",
-			url,
-			image:imageUrl
+		registerCb((response) => {
+			const { type } = response;
+			if (type === "playMedia") {
+				callback && callback(response);
+			}
 		});
+
+		platform === "ANDROID_APP" && webToNative.playMedia(JSON.stringify({ url, imageUrl }));
+
+		platform === "IOS_APP" &&
+			webToNativeIos.postMessage({
+				action: "playMedia",
+				url,
+				image: imageUrl,
+			});
 	}
 };
 
@@ -22,13 +33,21 @@ export const playMedia = (options = {}) => {
  *
  */
 export const pauseMedia = (options = {}) => {
+	const { callback } = options;
 	if (["ANDROID_APP", "IOS_APP"].includes(platform)) {
-		
+		registerCb((response) => {
+			const { type } = response;
+			if (type === "pauseMedia") {
+				callback && callback(response);
+			}
+		});
+
 		platform === "ANDROID_APP" && webToNative.pausePlaying();
 
-		platform === "IOS_APP" && webToNativeIos.postMessage({
-			"action": "pauseMedia"
-		});
+		platform === "IOS_APP" &&
+			webToNativeIos.postMessage({
+				action: "pauseMedia",
+			});
 	}
 };
 
@@ -38,11 +57,11 @@ export const pauseMedia = (options = {}) => {
  */
 export const stopMedia = (options = {}) => {
 	if (["ANDROID_APP", "IOS_APP"].includes(platform)) {
-		
 		platform === "ANDROID_APP" && webToNative.stopPlaying();
 
-		platform === "IOS_APP" && webToNativeIos.postMessage({
-			"action": "stopMedia"
-		});
+		platform === "IOS_APP" &&
+			webToNativeIos.postMessage({
+				action: "stopMedia",
+			});
 	}
 };
