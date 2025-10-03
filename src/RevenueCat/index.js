69ddede2999729777073fc84bd60eaed011a64d4
vijay <vijay.pandey@orufy.com>
2025-10-03 14:39:10 +0530

    add : callbacks

--- src/RevenueCat/index.js
+++ src/RevenueCat/index.js
@@ -1,8 +1,16 @@
 import { isIosApp, isAndroidApp } from "./../index";
-import { webToNative, webToNativeIos } from "../utills";
+import { registerCb, webToNative, webToNativeIos } from "../utills";
 
 export const configure = (options) => {
-	const { apiKey, userId } = options || {};
+	const { apiKey, userId, callback } = options || {};
+
+	registerCb((response) => {
+		const { type } = response;
+		if (type === "configure") {
+			callback && callback(response);
+		}
+	});
+
 	if (isAndroidApp) {
 		webToNative.configure(JSON.stringify(options));
 	} else if (isIosApp) {
@@ -14,7 +22,16 @@ export const configure = (options) => {
 	}
 };
 
-export const isInitialized = () => {
+export const isInitialized = (options) => {
+	const { callback } = options || {};
+
+	registerCb((response) => {
+		const { type } = response;
+		if (type === "isInitialized") {
+			callback && callback(response);
+		}
+	});
+
 	if (isAndroidApp) {
 		webToNative.isInitialized(JSON.stringify());
 	} else if (isIosApp) {
@@ -25,7 +42,15 @@ export const isInitialized = () => {
 };
 
 export const setUserId = (options) => {
-	const { userId } = options || {};
+	const { userId, callback } = options || {};
+
+	registerCb((response) => {
+		const { type } = response;
+		if (type === "setUserId") {
+			callback && callback(response);
+		}
+	});
+
 	if (isAndroidApp) {
 		webToNative.setUserId(JSON.stringify(options));
 	} else if (isIosApp) {
@@ -36,7 +61,16 @@ export const setUserId = (options) => {
 	}
 };
 
-export const getCustomerInfo = () => {
+export const getCustomerInfo = (options) => {
+	const { callback } = options || {};
+
+	registerCb((response) => {
+		const { type } = response;
+		if (type === "getCustomerInfo") {
+			callback && callback(response);
+		}
+	});
+
 	if (isAndroidApp) {
 		webToNative.getCustomerInfo(JSON.stringify());
 	} else if (isIosApp) {
@@ -48,7 +82,15 @@ export const getCustomerInfo = () => {
 };
 
 export const showPaywall = (options) => {
-	const { offeringId } = options || {};
+	const { offeringId, callback } = options || {};
+
+	registerCb((response) => {
+		const { type } = response;
+		if (type === "showPaywall") {
+			callback && callback(response);
+		}
+	});
+
 	if (isAndroidApp) {
 		webToNative.showPaywall(JSON.stringify(options));
 	} else if (isIosApp) {
@@ -60,7 +102,15 @@ export const showPaywall = (options) => {
 };
 
 export const makePurchase = (options) => {
-	const { productId } = options || {};
+	const { productId, callback } = options || {};
+
+	registerCb((response) => {
+		const { type } = response;
+		if (type === "makePurchase") {
+			callback && callback(response);
+		}
+	});
+
 	if (isAndroidApp) {
 		webToNative.makePurchase(JSON.stringify(options));
 	} else if (isIosApp) {
@@ -71,7 +121,16 @@ export const makePurchase = (options) => {
 	}
 };
 
-export const restorePurchase = () => {
+export const restorePurchase = (options) => {
+	const { callback } = options || {};
+
+	registerCb((response) => {
+		const { type } = response;
+		if (type === "restorePurchase") {
+			callback && callback(response);
+		}
+	});
+
 	if (isAndroidApp) {
 		webToNative.restorePurchase(JSON.stringify());
 	} else if (isIosApp) {
