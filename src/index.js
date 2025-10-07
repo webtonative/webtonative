2713617e14344a16a7cd4c99add595fe712d7b4d
vijay <vijay.pandey@orufy.com>
2025-10-07 16:31:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -356,13 +356,14 @@ export const disableScreenshot = (options) => {
 };
 
 export const customBackHandling = (options) => {
-	if (["ANDROID_APP","IOS_APP"].includes(platform)) {
-		const { enable=false } = options;
-		isAndroidApp && webToNative.customBackHandling(JSON.stringify({enable}));
-		isIosApp && webToNativeIos.postMessage({
-			"action": "customBackHandling",
-			enable
-		});
+	if (["ANDROID_APP", "IOS_APP"].includes(platform)) {
+		const { enable = false } = options;
+		isAndroidApp && webToNative.customBackHandling(JSON.stringify({ enable }));
+		isIosApp &&
+			webToNativeIos.postMessage({
+				action: "customBackHandling",
+				enable,
+			});
 	}
 };
 
@@ -460,7 +461,7 @@ export const showNativeComponents = (options) => {
 	if (isAndroidORIosApp) {
 		const { components } = options || {};
 
-		isAndroidApp && webToNative.checkPermission(JSON.stringify(options));
+		isAndroidApp && webToNative.showNativeComponents(JSON.stringify(options));
 		isIosApp &&
 			webToNativeIos.postMessage({
 				action: "showNativeComponents",
