3767ed7fb617f39bc94a8e6fcd608f4fc5741f85
vijay <vijay.pandey@orufy.com>
2026-02-20 15:24:05 +0530

    add : send bird function

---
+++ src/SendBird/index.tsx
@@ -0,0 +1,27 @@
+import { isIosApp, isAndroidApp } from "./../index";
+import { registerCb, webToNative, webToNativeIos } from "../utills";
+import { BaseResponse } from "../types";
+
+type IInitializeSendBird = {
+	userId: string;
+	callback?: (response: BaseResponse) => void;
+};
+export const initializeSendBird = (options: IInitializeSendBird) => {
+	const { userId, callback } = options || {};
+
+	registerCb((response) => {
+		const { type } = response;
+		if (type === "initializeSendBird") {
+			callback && callback(response);
+		}
+	});
+
+	if (isAndroidApp) {
+		webToNative.initializeSendBird(JSON.stringify(options));
+	} else if (isIosApp && webToNativeIos) {
+		webToNativeIos.postMessage({
+			action: "initializeSendBird",
+			userId,
+		});
+	}
+};
