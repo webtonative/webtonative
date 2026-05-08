a7f3804e9bc440be200ef1beeef90a1678c38d93
orufy-vijay <136577483+orufy-vijay@users.noreply.github.com>
2026-05-08 10:37:31 +0530

    add truecallerLogin fn

---
+++ src/Truecaller/index.ts
@@ -0,0 +1,40 @@
+import { platform, registerCb, webToNative, webToNativeIos } from "../utills";
+
+/**
+ * Initiates Truecaller login for Android and iOS
+ * @example
+ * truecallerLogin({
+ *   callback: (data) => {
+ *     console.log(data);
+ *   }
+ * });
+ */
+
+export interface TruecallerLoginOptions {
+	callback?: (response: any) => void;
+	scope?: string;
+}
+
+export const truecallerLogin = (options: TruecallerLoginOptions = {}): void => {
+	if (["ANDROID_APP", "IOS_APP"].includes(platform)) {
+		const { callback, ...rest } = options;
+
+		registerCb((response) => {
+			const { type } = response;
+			if (type === "truecallerLogin") {
+				callback && callback(response);
+			}
+		});
+
+		if (platform === "ANDROID_APP") {
+			webToNative.truecallerLogin && webToNative.truecallerLogin(JSON.stringify(rest));
+		}
+
+		if (platform === "IOS_APP" && webToNativeIos) {
+			webToNativeIos.postMessage({
+				action: "truecallerLogin",
+				...rest
+			});
+		}
+	}
+};
