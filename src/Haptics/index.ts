f62b4403829d139d74468f80ca062460e6027b5b
orufy-vijay <136577483+orufy-vijay@users.noreply.github.com>
2026-05-15 17:05:49 +0530

    Update index.ts

--- src/Haptics/index.ts
+++ src/Haptics/index.ts
@@ -1,4 +1,5 @@
-import { platform, webToNative, webToNativeIos } from "../utills";
+import { platform, webToNative, webToNativeIos, registerCb } from "../utills";
+import { BaseResponse } from "../types";
 import { HapticsOptions, HapticsIosMessage } from "./types";
 
 /**
@@ -14,5 +15,30 @@ export const trigger = (options: HapticsOptions = {}): void => {
 			} as HapticsIosMessage);
 		}
 		platform === "ANDROID_APP" && webToNative.haptikEffect(JSON.stringify(options));
+	}
+};
+
+interface IsHapticSupportedOptions {
+	callback?: (response: BaseResponse) => void;
+}
+
+export const isHapticSupported = (options: IsHapticSupportedOptions = {}): void => {
+	if (["ANDROID_APP", "IOS_APP"].includes(platform)) {
+		const { callback } = options;
+
+		registerCb((response: BaseResponse) => {
+			if (response && response.type === "isHapticSupported") {
+				callback && callback(response);
+			}
+		});
+
+		if (platform === "ANDROID_APP") {
+			webToNative.isHapticSupported && webToNative.isHapticSupported();
+		} else if (platform === "IOS_APP") {
+			webToNativeIos &&
+				webToNativeIos.postMessage({
+					action: "isHapticSupported",
+				});
+		}
 	}
 };
