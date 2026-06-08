b4e7ea67999d38ec3a3019206cc08687ccf8f9b8
orufy-vijay <136577483+orufy-vijay@users.noreply.github.com>
2026-06-08 11:27:10 +0530

    added

---
+++ src/AgeSafety/types.ts
@@ -0,0 +1,28 @@
+export interface AgeSafetyResponse {
+	type: string;
+	[key: string]: any;
+}
+
+export interface AgeSafetyCallback {
+	(response: AgeSafetyResponse): void;
+}
+
+export interface GetAgeSignalsOptions {
+	ageGates: number;
+	callback?: AgeSafetyCallback;
+}
+
+export interface NotifySignificantChangeOptions {
+	topicString: string;
+}
+
+export interface AgeSafetyIosMessage {
+	action: string;
+	ageGates?: number;
+	topicString?: string;
+}
+
+export interface AgeSafetyAndroidParams {
+	action: string;
+	ageGates?: number;
+}
